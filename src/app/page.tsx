cc0d08a8f4616b563c643965bd016217df3aa249
Dustin Kurtz <11kurtzd@gmail.com>
2024-04-27 07:28:20 -0400

    Added about and experience sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,84 @@
 import Image from "next/image";
+import GithubIcon from "../../public/github-142-svgrepo-com.svg";
+import LinkedInIcon from "../../public/linkedin-svgrepo-com.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex min-h-screen flex-col items-start bg-slate-900 text-slate-400 px-6">
+      <header className="my-12">
+        <div>
+          <h1 className="text-4xl text-slate-200 font-bold tracking-tight">Dustin Kurtz</h1>
+          <h3 className="text-slate-200 text-lg font-medium tracking-tight my-3.5">Software Engineer</h3>
+          <p>I build engaging, innovative software solutions</p>
+          <nav className="hidden lg:block my-20">
+            <ul className="w-max">
+              <li className="mt-5 hover:cursor-pointer">
+                <a className="group flex gap-4 items-center motion-reduce:transition-none" href="#about">
+                  <span className="border-b-2 border-slate-700 w-8 h-[1px] inline group-hover:w-16 group-hover:border-slate-200 transition-all duration-300 ease-in-out"></span>
+                  <span className="text-xs group-hover:text-slate-200 transition-all duration-300 ease-in-out">ABOUT</span>
+                </a>
+              </li>
+              <li className="mt-5 hover:cursor-pointer">
+                <a className="group flex gap-4 items-center motion-reduce:transition-none" href="#experience">
+                  <span className="border-b-2 border-slate-700 w-8 h-[1px] inline group-hover:w-16 group-hover:border-slate-200 transition-all duration-150 ease-in-out"></span>
+                  <span className="text-xs group-hover:text-slate-200 transition-all duration-150 ease-in-out">EXPERIENCE</span>
+                </a>
+              </li>
+              <li className="mt-5 hover:cursor-pointer">
+                <a className="group flex gap-4 items-center motion-reduce:transition-none" href="#projects">
+                  <span className="border-b-2 border-slate-700 w-8 h-[1px] inline group-hover:w-16 group-hover:border-slate-200 transition-all duration-150 ease-in-out"></span>
+                  <span className="text-xs group-hover:text-slate-200 transition-all duration-150 ease-in-out">PROJECTS</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <ul className="flex gap-4 items-center mt-7 px-1">
+          <li className="w-6 h-6">
+            <Image src={GithubIcon} alt="Github logo" />
+          </li>
+          <li className="w-8 h-8">
+            <Image src={LinkedInIcon} alt="LinkedIn logo" />
+          </li>
+        </ul>
+      </header>
+      <main>
+        <section id="about" className="mt-7 leading-customHeight">
+          <h3 className="text-slate-200 font-extrabold text-sm my-9 tracking-widest">ABOUT</h3>
+          <p>Back in 2012, I decided to try my hand at creating custom Tumblr themes and tumbled head first into the 
+            rabbit hole of coding and web development. Fast-forward to today, and I’ve had the privilege of building
+             software for an advertising agency, a start-up, a huge corporation, and a digital product studio.</p>
+           <p className="mt-4">My main focus these days is building accessible user interfaces for our customers at Klaviyo. I most enjoy
+             building software in the sweet spot where design and engineering meet — things that look good but are also
+              built well under the hood. In my free time, I've also released an online video course that covers everything
+               you need to know to build a web app with the Spotify API.</p>
+           <p className="mt-4">When I’m not at the computer, I’m usually rock climbing, reading, hanging out with my wife and two cats, 
+            or running around Hyrule searching for Korok Seeds.</p>
+        </section>
+        <section id="experience" className="mt-20">
+          <h3 className="text-slate-200 font-extrabold text-sm my-11 tracking-widest">EXPERIENCE</h3>
+          <ul>
+            <li>
+              <h5 className="text-xs">Oct 2023 - PRESENT</h5>
+              <div>
+                <h3 className="text-slate-200 font-extrabold text-sm my-2 tracking-widest">Software Engineer - <a>Mavin</a></h3>
+                <p className="text-xs">Build and maintain critical components used to construct Klaviyo’s frontend, across the whole product. 
+                  Work closely with cross-functional teams, including developers, designers, and product managers, to 
+                  implement and advocate for best practices in web accessibility.</p>
+              </div>
+            </li>
+            <li>
+              <h5 className="text-xs">Aug 2022 - Present</h5>
+              <div>
+                <h3 className="text-slate-200 font-medium text-md my-2 tracking-widest">Software Developer - <a>Self-Taught</a></h3>
+                <p className="text-xs">Build and maintain critical components used to construct Klaviyo’s frontend, across the whole product. 
+                  Work closely with cross-functional teams, including developers, designers, and product managers, to 
+                  implement and advocate for best practices in web accessibility.</p>
+              </div>
+            </li>
+          </ul>
+        </section>
+      </main>
+    </div>
   );
 }
